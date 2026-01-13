b390d65e13e262482f9e239aceb6bf5c37c67630
MaartentePoele <maarten.te.poele@hva.nl>
2026-01-13 12:40:25 +0100

    #6 Popup content opgemaakt en veranderd in een section

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const iButton = document.querySelector("#iButton");
+const xButton = document.querySelector("#xButton");
+const popup = document.querySelector("#popup");
+const body = document.querySelector("#mainBody");
+let pixelsScrolled = 0;
+
+window.addEventListener("scroll", function () {
+  pixelsScrolled = window.scrollY;
+  console.log(pixelsScrolled);
+});
+
+iButton.addEventListener("click", function () {
+  popup.classList.remove("visually-hidden");
+  popup.style.setProperty("--y", pixelsScrolled + "px");
+  body.classList.add("overflow-hidden");
+});
+
+xButton.addEventListener("click", function () {
+  popup.classList.add("visually-hidden");
+});
