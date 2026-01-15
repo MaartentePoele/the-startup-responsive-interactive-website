8603d8802503e8c68603be93de9a0beae088cd53
MaartentePoele <maarten.te.poele@hva.nl>
2026-01-15 15:58:20 +0100

    #6 Comments aan JS code toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
+// Declarations of CSS selectors
 const iButton = document.querySelector("#iButton");
 const xButton = document.querySelector("#xButton");
 const popup = document.querySelector("#popup");
 const body = document.querySelector("#mainBody");
 
+// Opens the popup and freezes background scrolling
 iButton.addEventListener("click", function () {
+  // Checks how far down the user scrolled
   const pixelsScrolled = window.scrollY;
 
+  // Puts the value of pixelsScrolled into --y for CSS use
   popup.style.setProperty("--y", pixelsScrolled + "px");
+
   popup.classList.remove("visually-hidden");
   body.classList.add("overflow-hidden");
 });
 
+// Closes the popup and unfreezes background scrolling
 xButton.addEventListener("click", function () {
   popup.classList.add("visually-hidden");
   body.classList.remove("overflow-hidden");
