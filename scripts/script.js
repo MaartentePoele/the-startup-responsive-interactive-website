60141a621b6271979e36ced829b72b92fdf3b828
MaartentePoele <maarten.te.poele@hva.nl>
2026-01-16 11:37:26 +0100

    #6 Animation aan i-icoon toegevoegd als de gebruiker op de label klikt.

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,8 @@ const iButton = document.querySelector("#iButton");
 const xButton = document.querySelector("#xButton");
 const popup = document.querySelector("#popup");
 const body = document.querySelector("#mainBody");
+const label = document.querySelector(".energy-label-a");
+const iIcon = document.querySelector(".i-icon");
 
 // Opens the popup and freezes background scrolling
 iButton.addEventListener("click", function () {
@@ -20,4 +22,14 @@ iButton.addEventListener("click", function () {
 xButton.addEventListener("click", function () {
   popup.classList.add("visually-hidden");
   body.classList.remove("overflow-hidden");
+});
+
+// Gives an animation to the i-icon when the user clicks on the label
+label.addEventListener("click", function () {
+  iIcon.classList.add("i-animation");
+
+  // Waits for the animation to end before removing the class with the animation
+  iIcon.addEventListener("animationend", function () {
+    iIcon.classList.remove("i-animation");
+  });
 });
